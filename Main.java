bcf1f5f1dbdff683b700d83c72ec39bd252b94e1
Dilpreet <155355461+Dilpreet-Sandhu@users.noreply.github.com>
2025-10-08 12:21:30 +0530

    added main

--- Main.java
+++ Main.java
@@ -14,40 +14,61 @@ public class Main {
     }
 
     sc.close();
-
+    
   }
-  public static void solve(Scanner sc) {
-
+ public static void solve(Scanner sc) {
     int n = sc.nextInt();
-
-    int one = 0; 
-    int minus = 0;
-
+    int[] arr = new int[n];
+    boolean allSame = true;
     for (int i = 0;i < n;i++) {
-      int x = sc.nextInt();
-      if (x == -1) {
-        minus++;
-      }else {
-        one++;
-      }
+
+      arr[i] = sc.nextInt();
+
+      if (i > 0 && arr[i - 1] != arr[i]) allSame = false;
+
     }
-
-    if (minus > one) {
-
-      int cnt = 0;
-      while (minus > one) {
-        minus--;
-        one++;
-        cnt++;
-      }
-
-      System.out.println(cnt);
+    
+    if (allSame) {
+      System.out.println("NO");
       return;
     }
 
-    System.out.println(0);
+    reverse(arr,n);
+
+    if (arr[0] == arr[1]) {
+      for (int i = 2;i < n;i++) {
+        if (arr[i] != arr[0]) {
+          int temp = arr[i];
+          arr[i] = arr[1];
+          arr[1] = temp;
+          break;
+        }
+      }
+    }
+
+    System.out.println("YES");
+    for (int i : arr) System.out.print(i + " ");
+    System.out.println();
+
     
+    
+}
+
+public static void reverse(int[] arr,int n) {
+  int i = 0;
+  int j = n - 1;
+  while (i <= j) {
+
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+
+    i++;
+    j--;
+
   }
+}
+
   
 
  
